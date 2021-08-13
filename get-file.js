fab19018fb9838ebca9517bd6022261510aabe90
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-08-13 14:51:44 +0300

    sync function

--- get-file.js
+++ get-file.js
@@ -2,11 +2,12 @@
 const fs = require('fs')
 const determine_mime_type = require('determine-mime-type')
 const error_responses = require('error-responses')
-module.exports = async function get_file(pathname)
-{	try
-	{	return {
+module.exports = function get_file(pathname) {
+    try {
+        return {
 		status: 200,
 		mimetype: determine_mime_type(pathname),
 		data: fs.createReadStream(pathname),
-		headers: [], }}
-	catch(e) { return error_responses.not_found }}
+		headers: [], }
+    } catch(e) { return error_responses.not_found }
+}
